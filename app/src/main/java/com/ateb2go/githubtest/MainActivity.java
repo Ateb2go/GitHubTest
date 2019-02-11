a27958883c02e2869699baee90e4817283ab4706
Ateb2go <47515534+Ateb2go@users.noreply.github.com>
2019-02-11 15:58:08 +0900

    Update MainActivity.java

--- app/src/main/java/com/ateb2go/githubtest/MainActivity.java
+++ app/src/main/java/com/ateb2go/githubtest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Babuyai");
+        //좋아요yee
     }
 }
